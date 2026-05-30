744757f56ebc5297034b36f29aa62a836feb8cbd
qqontheskyshell <qqontheskyshell@gmail.com>
2026-05-30 15:47:54 +0900

    Create arcOSontheShell.sh

---
+++ arcOSontheShell.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+sshinto(){
+cmd=(.arcOSBaseKit & arcOSLoop *)
+runssh(){ 
+ssh -i “${SSH_KEY}” -p “${PORT}” -o StrictHostKeyChecking=accept-new “root@$QQLOCAL” “$*”
+}
+
+runssh “${cmd}” &
+}
