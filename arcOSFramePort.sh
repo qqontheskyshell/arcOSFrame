7edb92ae3358d1eecb3e0a2cdefc2bea91ecc3b4
qqontheskyshell <qqontheskyshell@gmail.com>
2026-05-30 15:44:21 +0900

    Update arcOSFramePort.sh

--- arcOSFramePort.sh
+++ arcOSFramePort.sh
@@ -35,10 +35,21 @@ BEGIN { IGNORECASE=1 }
 }
 '
 
-usbProductID=$(system_profiler SPUSBDataType 2>/dev/null | awk ' BEGIN{IGNORECASE=1} /^[[:space:]]{4}[^:]+:$/ {dev=$0} /Manufacturer: Apple|Product ID:|Vendor ID:/ {
-    if (dev != "") print dev "\n" $0
-}
-' | sed '/^$/d' | awk '{print $3}')
-
+usbProductID=$(
+  system_profiler SPUSBDataType 2>/dev/null |
+  awk '
+    BEGIN { IGNORECASE=1 }
+    /^[[:space:]]{4}[^:]+:$/ { dev = $0; isApple = 0 }
+    /Manufacturer: Apple/ { isApple = 1 }
+    /Product ID:/ && isApple {
+      for (i = 1; i <= NF; i++) {
+        if ($i ~ /^0x[0-9a-fA-F]+$/) {
+          print $i
+          break
+        }
+      }
+    }
+  '
+)
 
 }
