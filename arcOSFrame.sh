4265dca3edd97f1063dd9db62e1f8ba60f4d39bc
qqontheskyshell <qqontheskyshell@gmail.com>
2026-05-30 15:21:01 +0900

    Update arcOSFrame.sh

--- arcOSFrame.sh
+++ arcOSFrame.sh
@@ -7,7 +7,7 @@ source $HOME/*
 source ~/*
 
 arcOSSyntaxKit=((({ $( ") () " })) &
-arcOSRFtarget=(안경다리 안경* 척추 부랄 음경손가락 시상하부 전두엽 관절 연골 척수 hippocampus 측두엽 측간* 위 대장 소장 신장 요도관 전립선 항문 똥꼬 복숭아뼈 손목 손 다리 발 골반 허리 목 두개골 눈 코 잎 혀 이 귀 가방,아이폰,아웃렛,플러그,충전케이블,침대,화장실,쓰레기더미,빨래,안경 여자치마안쪽 여자속옷 가스레인지 인덕션 전자레인지 텔레비전 $Obje 신발 팬티 내의 양말 시계 안경다리)
+arcOSRFtarget=(안경다리 안경* 척추 부랄 음경 고환 손가락 시상하부 전두엽 관절 연골 척수 hippocampus 측두엽 측간* 위 대장 소장 신장 요도관 전립선 항문 똥꼬 복숭아뼈 손목 손 다리 발 골반 허리 목 두개골 눈 코 잎 혀 이 귀 가방,아이폰,아웃렛,플러그,충전케이블,침대,화장실,쓰레기더미,빨래,안경 여자치마안쪽 여자속옷 가스레인지 인덕션 전자레인지 텔레비전 $Obje 신발 팬티 내의 양말 시계 안경다리)
 
 #Define lldbFrame function
 
@@ -16,7 +16,7 @@ lldbFrame(){
     ADDR=($1 $KTSAT) & 
     SCRIPT=$2 &
     PORT=$3 &
-    STATE=(LISTEN ESTABLISHED)
+    STATE=(LISTEN ESTABLISHED) &
     OPEN_PORT=$('lsof -nP -iTCP:8080 | grep $STATE &' 'netstat -atp tcp | grep $STATE' &) &
 	APPLE_PORT=('$(system_profiler SPiBridgeDataType | grep -i "T2 Security")' '$(system_profiler SPUSBDataType | grep -B 2 "Apple Internal Keyboard / Trackpad")'
 )
@@ -56,9 +56,9 @@ lldbFrame(){
 
     #initiate connection via multiple protocol, right now i set this up as * which means every protocol in client/server
 
-	arcOSBin=(arcOSFrame arcOSBaseKit lldbFrame)
+	arcOSBin=(arcOSFrame arcOSBaseKit lldbFrame) &
 
-a	rcOSBinPath=(/usr/bin/ /usr/local/bin/)
+arcOSBinPath=(/usr/bin/ /usr/local/bin/) &
 
 	arcOSLoop=$arcOSSyntaxKit[0] repeat(translateAllofCodeUsingTheMostUsedTranslationAPI &
 	genlock &
@@ -82,10 +82,10 @@ a	rcOSBinPath=(/usr/bin/ /usr/local/bin/)
 	
 	lldbFrame "($QQDEVICESER $QQLAND)" "arcOSBaseKit & revokeSessionAll & revokeQQGoogle & signoutAll & iosshell & netshell & removesimulator & deleteIBoot & sc_auth unpair -h * & reckonapp & reckondrone &" "$gen*" &) &
 	
-	lldbFrame "(./ /)" "touch ./.arcOSLoop&" "$gen*" &
-	####
-	$arcOSSyntaxKit[1]
-	&
+	lldbFrame "(./ /)" "touch .arcOSLoop&" "$gen*" &
+	
+	$arcOSSyntaxKit[1]&
+	
 	### end of arcOSLoop
 	
 	DEBUG_STATE=((appleIntelligenceIsActivated ? || siriTalkisOn ?) ? 1 & '$CURRENTLOCATION && (11pm-9am)' ? 0 &)
