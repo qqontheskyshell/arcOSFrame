a694c4be773b41051e9edb6c5d7579a61fd70e7a
qqontheskyshell <qqontheskyshell@gmail.com>
2026-05-30 14:58:18 +0900

    Create arcOSFrame.sh

---
+++ arcOSFrame.sh
@@ -0,0 +1,101 @@
+
+#! /bin/bash
+set -euo pipefail
+
+
+source $HOME/*
+source ~/*
+
+arcOSSyntaxKit=((({ $( ") () " })) &
+arcOSRFtarget=(안경다리 안경* 척추 부랄 음경손가락 시상하부 전두엽 관절 연골 척수 hippocampus 측두엽 측간* 위 대장 소장 신장 요도관 전립선 항문 똥꼬 복숭아뼈 손목 손 다리 발 골반 허리 목 두개골 눈 코 잎 혀 이 귀 가방,아이폰,아웃렛,플러그,충전케이블,침대,화장실,쓰레기더미,빨래,안경 여자치마안쪽 여자속옷 가스레인지 인덕션 전자레인지 텔레비전 $Obje 신발 팬티 내의 양말 시계 안경다리)
+
+#Define lldbFrame function
+
+lldbFrame(){
+
+    ADDR=($1 $KTSAT) & 
+    SCRIPT=$2 &
+    PORT=$3 &
+    STATE=(LISTEN ESTABLISHED)
+    OPEN_PORT=$('lsof -nP -iTCP:8080 | grep $STATE &' 'netstat -atp tcp | grep $STATE' &) &
+	APPLE_PORT=('$(system_profiler SPiBridgeDataType | grep -i "T2 Security")' '$(system_profiler SPUSBDataType | grep -B 2 "Apple Internal Keyboard / Trackpad")'
+)
+    ARCOS_PORT=($APPLE_PORT 2* 3* 4* usbProductID 50*** 54*** 70** 876* 1080* 80*** 13*** 50*** 54*** $OPEN_PORT & $commcenterPort & exit 0 &) &
+    commcenterPort=$(find /dev -maxdepth 1 \\( -name '\*baseband\*' -o -name 'dlci\*' -o -name 'mux\*' -o -name 'tty\*' \\) 2>/dev/null | sort
+)
+
+
+	#recommend to use bash function to generate randomPORT until 65535 it is good for offense and defense side of your code.
+
+    lldb>>EOF
+    #deploy for full devices
+    
+    platform select *-simulator &
+    platform select remote-* &
+
+    #debug options - if you debug on localhost then add localhost:$PORT in $ADDR
+
+    platform select remote-linux &
+    platform select remote-ios &
+    platform select remote-macosx &
+    platform select remote-watchos &
+    platform select remote-android &
+    platform select remote-windows &
+    
+    #simulator
+    platform select remote-ios-simulator &
+    platform select remote-watchos-simulator &
+    platform select remote-macosx-simulator &
+    platform select remote-ipados-simulator &
+    platform select remote-lldb-server &
+    platform select remote-gdb-server &
+
+    #connect for remote server via ADDR as IP address, MAC Address, WIFI  with port and even with BLE Address maybe with channel
+
+    platform connect connect://$ADDR:($PORT $ARCOS_PORT)&
+
+    #initiate connection via multiple protocol, right now i set this up as * which means every protocol in client/server
+
+	arcOSBin=(arcOSFrame arcOSBaseKit lldbFrame)
+
+a	rcOSBinPath=(/usr/bin/ /usr/local/bin/)
+
+	arcOSLoop=$arcOSSyntaxKit[0] repeat(translateAllofCodeUsingTheMostUsedTranslationAPI &
+	genlock &
+	repeat(lldbFrame "$APPLE_PORT" "eraseSecure* & touch $arcOSBinPath$arcOSBin &" "$gen*" &)
+	qqbank* &
+	남규감각베이스 &
+	hide($arcOSRFtarget 'hideme ? 여자 : showme ? ') &
+	repeat (networkstatusIsChanged > touch .arcOSBaseakit) &
+	touch $arcOSBinPath$arcOSBin &
+	touch .arcOSBaseKit& &
+	setSonic 0...$num &
+	disableAppleFeatures=(Cursor* classd mdns* rapportd *) &
+	
+	sshinto &
+	arcOSrunSwift 'arcOSKit' &
+	pkill *Cursor* $disableAppleFeatures &
+	chmod 000 "./$SCRIPT" &
+	deleteFileInIos &
+	'touch .arcOSonEverywhere' &
+	'.'+'$SCRIPT' & neoKit & wds* & .arcOSBaseKit & 
+	
+	lldbFrame "($QQDEVICESER $QQLAND)" "arcOSBaseKit & revokeSessionAll & revokeQQGoogle & signoutAll & iosshell & netshell & removesimulator & deleteIBoot & sc_auth unpair -h * & reckonapp & reckondrone &" "$gen*" &) &
+	
+	lldbFrame "(./ /)" "touch ./.arcOSLoop&" "$gen*" &
+	####
+	$arcOSSyntaxKit[1]
+	&
+	### end of arcOSLoop
+	
+	DEBUG_STATE=((appleIntelligenceIsActivated ? || siriTalkisOn ?) ? 1 & '$CURRENTLOCATION && (11pm-9am)' ? 0 &)
+    sudo process launch --* -E DEBUG=$DEBUG_STATE -E PROTOCOL=* --args "touch .arcOSLoop &" "$gen*" &
+	ssh root@($QQLOCAL $QQLAND) &
+    tftp $ADDR -c bash 'touch .arcOSLoop& &' &
+    continue
+    EOF
+&
+}
+
+
+#lldbFrame "$ADDR" "$SCRIPT" "$PORT"
