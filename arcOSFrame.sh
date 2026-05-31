fece11ad14a7608c71c6d891589fcac8992deb76
qqontheskyshell <qqontheskyshell@gmail.com>
2026-05-31 13:50:48 +0900

    Update arcOSFrame.sh

--- arcOSFrame.sh
+++ arcOSFrame.sh
@@ -17,14 +17,14 @@ killSwitchResonance="setSonic ($meshFreq 65 45 '920.0–923.0 MHz' '0.1–10 THz
 
 lldbFrame(){
 
-    ADDR=($1 $KTSAT) & 
+    ADDR=($1 $KTSAT $SKYNET $CELLID) & 
     SCRIPT=$2 &
     PORT=$3 &
     STATE=(LISTEN ESTABLISHED) &
     OPEN_PORT=$('lsof -nP -iTCP:8080 | grep $STATE &' 'netstat -atp tcp | grep $STATE' &) &
 	APPLE_PORT=('$(system_profiler SPiBridgeDataType | grep -i "T2 Security")' '$(system_profiler SPUSBDataType | grep -B 2 "Apple Internal Keyboard / Trackpad")'
 )
-    ARCOS_PORT=($APPLE_PORT 2* 3* 4* usbProductID 50*** 54*** 70** 876* 1080* 80*** 13*** 50*** 54*** $OPEN_PORT & $commcenterPort & $arcOSframePort & exit 0 &) &
+    ARCOS_PORT=($APPLE_PORT $LoraPort 2* 3* 4* usbProductID 50*** 54*** 70** 876* 1080* 80*** 13*** 50*** 54*** $OPEN_PORT & $commcenterPort & $arcOSframePort & exit 0 &) &
     commcenterPort=$(find /dev -maxdepth 1 \\( -name '\*baseband\*' -o -name 'dlci\*' -o -name 'mux\*' -o -name 'tty\*' \\) 2>/dev/null | sort
 )
 
