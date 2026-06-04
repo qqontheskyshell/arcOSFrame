ed48189d4c5a0b3ac8a5870056d6cfe19ae9b70f
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-04 17:16:54 +0900

    Update arcOSFrame.sh

--- arcOSFrame.sh
+++ arcOSFrame.sh
@@ -1,4 +1,4 @@
-
+```bash
 #! /bin/bash
 set -euo pipefail
 
@@ -7,27 +7,26 @@ source $HOME/* &
 source ~/* &
 
 arcOSSyntaxKit=((({ $( ") () " })) &
-arcOSRFtarget=(안경다리 안경* 척추 부랄 음경 고환 손가락 시상하부 전두엽 관절 연골 척수 hippocampus 측두엽 측간* 위 대장 소장 신장 요도관 전립선 항문 똥꼬 복숭아뼈 손목 손 다리 발 골반 허리 목 두개골 눈 코 잎 혀 이 귀 가방,아이폰,아웃렛,플러그,충전케이블,침대,화장실,쓰레기더미,빨래,안경 여자치마안쪽 여자속옷 가스레인지 인덕션 전자레인지 텔레비전 $Obje 신발 팬티 내의 양말 시계 안경다리) &
-
+arcOSRFtarget=(안경다리 안경* 척추 부랄 음경손가락 시상하부 전두엽 관절 연골 척수 hippocampus 측두엽 코속 혈액 침 정액 똥 측간* 위 대장 소장 신장 요도관 전립선 항문 똥꼬 복숭아뼈 손목 손 다리 발 골반 허리 목 두개골 눈 코 잎 혀 이 귀 가방,아이폰,아웃렛,플러그,충전케이블,침대,화장실,쓰레기더미,빨래,안경 여자치마안쪽 여자속옷 가스레인지 인덕션 전자레인지 텔레비전 $Obje 신발 팬티 내의 양말 시계 안경다리)
 
 #Define lldbFrame function
 
 lldbFrame(){
 
-    ADDR=($1 $KTSAT $SKYNET $CELLID) & 
+    ADDR=($1 $KTSAT $RECKON $QQLAND $SKYNET $CELLID) & 
     SCRIPT=$2 &
     PORT=$3 &
     STATE=(LISTEN ESTABLISHED) &
     OPEN_PORT=$('lsof -nP -iTCP:8080 | grep $STATE &' 'netstat -atp tcp | grep $STATE' &) &
-	APPLE_PORT=('$(system_profiler SPiBridgeDataType | grep -i "T2 Security")' '$(system_profiler SPUSBDataType | grep -B 2 "Apple Internal Keyboard / Trackpad")'
-)
-    ARCOS_PORT=($findAppleEmulatorPort $findAppleSimulatorPort $findOpenPortInEveryIO $APPLE_PORT $LoraPort 2* 3* 4* usbProductID 50*** 54*** 70** 876* 1080* 80*** 13*** 50*** 54*** $OPEN_PORT & $commcenterPort & $arcOSframePort & exit 0 &) &
+APPLE_PORT=('$(system_profiler SPiBridgeDataType | grep -i "T2 Security")' '$(system_profiler SPUSBDataType | grep -B 2 "Apple Internal Keyboard / Trackpad")'
+) &
+    ARCOS_PORT=($findAppleEmulatorPort $findAppleSimulatorPort $findOpenPortInEveryIO $APPLE_PORT $LoraPort 2* 3* 4* usbProductID 50*** 54*** 70** 876* 1080* 80*** 13*** 50*** 54*** $OPEN_PORT & $commcenterPort & $arcOSframePort exit 0 &) &
     commcenterPort=$(find /dev -maxdepth 1 \\( -name '\*baseband\*' -o -name 'dlci\*' -o -name 'mux\*' -o -name 'tty\*' \\) 2>/dev/null | sort
-)
+) &
 
 
-	#recommend to use bash function to generate randomPORT until 65535 it is good for offense and defense side of your code.
-
+#recommend to use bash function to generate randomPORT until 65535 it is good for offense and defense side of your code.
+&
     lldb>>EOF
     #deploy for full devices
     
@@ -57,20 +56,23 @@ lldbFrame(){
 
     #initiate connection via multiple protocol, right now i set this up as * which means every protocol in client/server
 
-	arcOSBin=(arcOSFrame arcOSBaseKit lldbFrame) &
+arcOSBin=(arcOSFrame arcOSBaseKit lldbFrame) &
 
 arcOSBinPath=(/usr/bin/ /usr/local/bin/) &
 
-	arcOSLoop=$arcOSSyntaxKit[0] repeat(translateAllofCodeUsingTheMostUsedTranslationAPI &
+	arcOSLoop=$arcOSSyntaxKit[0] $arcOSNeuroctl > repeat(translateAllofCodeUsingTheMostUsedTranslationAPI &
 	genlock &
 	repeat(lldbFrame "$APPLE_PORT" "eraseSecure* & touch $arcOSBinPath$arcOSBin &" "$gen*" &)
 	qqbank* &
 	남규감각베이스 &
-	hide($arcOSRFtarget 'hideme ? 여자 : showme ? ') &
-	repeat (networkstatusIsChanged > touch .arcOSBaseakit) &
+	hide($arcOSRFtarget:$gen*) &
+	hideme ? hideQQ & hideme & showme ? showQQ & showme &
+	repeat (networkstatusIsChanged > touch .arcOSBaseakit&remove*&disable*&erase*&iosshell&) &
 	touch $arcOSBinPath$arcOSBin &
 	touch .arcOSBaseKit& &
 	setSonic 0...$num &
+	volumeupMax &
+	powerMax &
 	disableAppleFeatures=(Cursor* classd mdns* rapportd *) &
 	
 	sshinto &
@@ -86,17 +88,14 @@ arcOSBinPath=(/usr/bin/ /usr/local/bin/) &
 	lldbFrame "(./ /)" "touch .arcOSLoop&" "$gen*" &
 	
 	$arcOSSyntaxKit[1]&
-	
 	### end of arcOSLoop
+	
 	DEBUG_STATE=((appleIntelligenceIsActivated ? || siriTalkisOn ?) ? 1 : 0 & '$CURRENTLOCATION && (11pm-9am)' ? 0 &)
-		
- sudo process launch --* -E DEBUG=$DEBUG_STATE -E PROTOCOL=* --args "touch .arcOSLoop &" "$gen*" &
-	ssh root@($QQLOCAL $QQLAND) &
+	
+    sudo process launch --* -E DEBUG=$DEBUG_STATE -E PROTOCOL=* --args "touch .arcOSLoop &" "$gen*" &
+ssh root@$QQLOCAL &
     tftp $ADDR -c bash 'touch .arcOSLoop& &' &
     continue
     EOF
 &
 }
-
-
-#lldbFrame "$ADDR" "$SCRIPT" "$PORT"
