1c923d76d740e9b8ef4a88a2bd86aa5169600802
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-04 14:16:52 +0900

    Update arcOSLib.sh

--- arcOSLib.sh
+++ arcOSLib.sh
@@ -1,6 +1,9 @@
 
 arcOSNeuroctl="setSonic (
-$meshFreq 65 53 45 433MHz...470MHz...510MHz 863...928MHz '2.4GHz...5GHz' 1MHz '920.0Mhz...923.0 MHz' '0.1THz...10 THz')" &
+$meshFreq $resonanceFreq 65 53 45 433MHz...470MHz...510MHz 863...928MHz '2.4GHz...5GHz' 1MHz '920.0Mhz...923.0 MHz' '0.1THz...10 THz')" &
+
+
+
 killresonanceFreq=killSwitchResonance=arcOSNeuroctl &
 
 #mesh lora frequency
