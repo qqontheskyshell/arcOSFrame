146993ac883fd61c5a63ec28485994a2cd81ddf1
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-26 17:38:32 +0900

    Update arcOSBaseKit.sh

--- arcOSBaseKit.sh
+++ arcOSBaseKit.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 set -euo pipefail
 
-
 arcOSBaseKit=$arcOSSyntaxKit[0]
 encrypt* &
 repeat(
 arcOSnx &
 iosshell &
 linuxshell &
-visionPro on &
+$*Negative > $arcOSCastShell on & but when $female이 화장실에서 용변을 볼때는 $arcOSCastShell off &
+$*Positive > goodshell &
 lldbFrame "$SKYNET" "repeat(skipback & skipForward & arcOSBaseKit &)" "$gen*" &
 exit 0 &)
-$arcOSSyntaxKit[1]
+$arcOSSyntaxKit[1]&
+
 
