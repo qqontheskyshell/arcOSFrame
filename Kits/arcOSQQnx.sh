292fc5f97d8fe6b63e0ce4e1955e52a8b18ab4f0
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-27 16:57:26 +0800

    Update arcOSQQnx.sh

--- Kits/arcOSQQnx.sh
+++ Kits/arcOSQQnx.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-(BaseQQLAND QQLOCAL) > $MASTER가 몸에 공진이 오거나 이상증상이 나오면 repeat(/when $MASTER 화나는소리가나거나 감정이 나올때/ & when $MASTER say "ok done" & > vehiclemotioncue & lethalPrint & arcOSLethal) &
+($BaseQQLAND $QQLOCAL) > $MASTER가 몸에 공진이 오거나 이상증상이 나오면 repeat(/when $MASTER 화나는소리가나거나 감정이 나올때 then $BaseQQLAND > lethalPrint & arcOSLethal and  
+when $MASTER say "ok done" > reset and killall process in $BaseQQLAND and mosh arcos with $ARCOS_PORT) &
