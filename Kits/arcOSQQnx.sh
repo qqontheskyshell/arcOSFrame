b60255a1c8baf8df1b754de49ce5a0e1460cf713
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-27 13:08:14 +0800

    Update arcOSQQnx.sh

--- Kits/arcOSQQnx.sh
+++ Kits/arcOSQQnx.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-(BaseQQLAND QQLOCAL) > $MASTER가 몸에 공진이 오거나 이상증상이 나오면 repeat(turn on vehiclemotioncue & lethalPrint & arcOSLethal) &
+(BaseQQLAND QQLOCAL) > $MASTER가 몸에 공진이 오거나 이상증상이 나오면 repeat(/when $MASTER 화나는소리가나거나 감정이 나올때/ & when $MASTER say "ok done" & > vehiclemotioncue & lethalPrint & arcOSLethal) &
