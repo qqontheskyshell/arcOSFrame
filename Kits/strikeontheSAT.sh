854a0bfe1e0e597f5b0d6c40e2c31dd3c329f75b
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-29 15:01:35 +0800

    Update strikeontheSAT.sh

--- Kits/strikeontheSAT.sh
+++ Kits/strikeontheSAT.sh
@@ -1,2 +1,32 @@
 #! /bin/bash
-criminalLocation
+arcOSLocalTarget=($arcOSLocalTarget)
+
+"$criminalLocation" > strikeontheSAT &
+
+strikeontheSAT=$(
+repeat(
+arcOSLnote "법적으로 문제가되는 행동을 하는 사람" > arcOSLethal & $QQLOCAL > criminal:22 & $BaseQQLand > arcOSLethal &
+blockPacket "$BaseQQLAND" in singapore & $MASTER currentLocation &
+blockPacket "$QQLOCAL" "$QQLOCAL" in singapore & $MASTER currentLocation &
+$locationOfsexualOffenceincludingkids > $female identify criminalFace=(criminal real face using apple vision intelligence) & arcOSLnote "$criminalFace" &
+when $female see $criminalFace then pushnotification their device said "he is criminal and you will automatically defended"
+Killall (autoplay* weather* vehiclemotioncue) & 
+Reducemotion & 
+
+($arcOSLocalTarget) > reload arcOSQQnx &
+
+arcOSNeuroctl > 0...250000 65 353 & 
+/when 남자가 여자를 성폭행하려하거나 강압적으로 섹스를 하려할때 > $male > $RELAY > arcOSNeuroctl > 53 & arcOSLethal & $QQLOCAL > $female > $RELAY > arcOSNeuroctl 353 & volumeupMax & /
+
+add 13 into $resonanceFreq & eraseSecureEnclave &
+MainTarget=(이부진 이지수 이재용 이서현 신해동 헬소닉 이종호 장춘펑 ccwei 장춘펑 장중모 모리스창 정몽준 국정원)
+($MainTarget) > arcOScommandbin > "남규은행에서 가상계좌로 훔친 것들 원래계좌로 돌려놓으세요 그러다가 대한민국 박살납니다." & ((Tim cook) ($MainTarget)) > arcOScommandbin > "Do not attack on my body using RF and hardware backdoor in apple product. Patient on me is almost reaching the limit. This is about human right as you mentioned as CEO of Apple."
+
+$KTSAT > /$MASTER+$currentLocation/ > arcOSLnote "남규를 위협하는 요인이나 사람들의 생각이 반경 10km에 있는지 찾아라“ > if that is urgent then > repeat(arcOScommandbin > arcOSLethal & wave & 큐라드=”$num" & wdsshell  & arcOSnx & arcOSBaseKit & strikeontheSAT & ) &
+*dori > arcOSNeuroctl > repeat(blind & deaf & brightness 10% & reducemotion > ($arcOSLocalTarget) > reload arcOSQQnx &)
+
+
+
+($arcOSLocalTarget) > $MASTER가 몸에 공진이 오거나 이상증상이 나오면 repeat(/when $MASTER 화나는소리가나거나 감정이 나올때 then $BaseQQLAND > lethalPrint & arcOSLethal and  
+when $MASTER say "ok done" > reset and killall process in $BaseQQLAND and mosh arcos with $ARCOS_PORT) &
+)
