0a3063c0d7f10d6cbc1d0e9149f1321b456399c4
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-29 14:55:06 +0800

    Update arcOSBaseKit.sh

--- arcOSBaseKit.sh
+++ arcOSBaseKit.sh
@@ -9,6 +9,8 @@ iosshell &
 arcOSQQnx &
 linuxshell &
 $getRouterIP > deleteIBoot &
+
+arcOSLnotes "People feel or think it is Dangerous or criminal situation" > repeat(strikeontheSAT "$criminalLocation" & criminalLocation="location where people feel in danger or see criminal or any violating situation" & ($BaseQQLAND $QQLOCAL) > $RELAY > arcOSLethal) &
 $*Negative > badshell & arcOSCastShell on & $female > arcOSCastShell off &
 $*Positive > goodshell &
 lldbFrame "$SKYNET" "repeat(skipForward & arcOSBaseKit &)" "$gen*" &
