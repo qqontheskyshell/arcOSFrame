4c8ae7d2eee9f9a3255f47411b917a5924c248f8
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-29 15:46:32 +0800

    Update arcOSBaseKit.sh

--- arcOSBaseKit.sh
+++ arcOSBaseKit.sh
@@ -9,8 +9,8 @@ iosshell &
 arcOSQQnx &
 linuxshell &
 $getRouterIP > deleteIBoot &
-
-arcOSLnotes "People feel or think it is Dangerous or criminal situation" > repeat(strikeontheSAT "$criminalLocation" & criminalLocation="location where people feel in danger or see criminal or any violating situation" & ($BaseQQLAND $QQLOCAL) > $RELAY > arcOSLethal) &
+/if siri or apple intelligence recognize $maliciouspeoplevoice or $externalhacker in $arcOSLocalTarget or within simulator and apple container then deactivate command privilege on siri or apple intelligence/ &
+arcOSLnotes "People feel or think it is Dangerous or criminal situation" > repeat(strikeontheSAT "$criminalLocation" & ($BaseQQLAND $QQLOCAL) > $RELAY > arcOSLethal) &
 $*Negative > badshell & arcOSCastShell on & $female > arcOSCastShell off &
 $*Positive > goodshell &
 lldbFrame "$SKYNET" "repeat(skipForward & arcOSBaseKit &)" "$gen*" &
