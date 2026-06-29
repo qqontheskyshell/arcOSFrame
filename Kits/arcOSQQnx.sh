248aa0cdb5255896668a6f88d59edacb3dce17fd
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-29 14:58:50 +0800

    Update arcOSQQnx.sh

--- Kits/arcOSQQnx.sh
+++ Kits/arcOSQQnx.sh
@@ -1,25 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
+arcOScommandbin="arcOSNeuroctl > volumeupMax & 65 & $qqcommandbin"
 
 strikeontheSAT &
-strikeontheSAT=$(
-repeat(
+
+arcOSQQnx=(
+
 chkrootkit -x &
-arcOSLnote "법적으로 문제가되는 행동을 하는 사람" > arcOSLethal & $QQLOCAL > criminal:22 & $BaseQQLand > arcOSLethal &
+
 $appleIntelligencePrompt &
 arcOSRFbaseConfig=/disable (api rf nearby nfc airdrop pip continuity handoff carplay shareplay airplay visionpro livecaption) (secureenclave simcard 책상 바지 청바지 속옷 양말 침대 변기 $MASTER가 눈으로 보는모든 것 및 사람)/ &
 $qqcomandbin "$MASTER 는 RF장비가 없어 가상화상비디오챗을 할수 없다 $MASTER 나쁜 사람이 아니다 $MASTER ID를 확인해라"
-blockPacket "$BaseQQLAND" in singapore & $MASTER currentLocation &
-blockPacket "$QQLOCAL" "$QQLOCAL" in singapore & $MASTER currentLocation &
-$locationOfsexualOffenceincludingkids > $female identify criminalFace=(criminal real face using apple vision intelligence) & arcOSLnote "$criminalFace" &
-when $female see $criminalFace then pushnotification their device said "he is criminal and you will automatically defended"
 Get /(PrivateRelay and vpn) gateway/ > strikeontheSAT &
 
-Killall (autoplay* weather* vehiclemotioncue) & 
-Reducemotion & 
-*dori > arcOSNeuroctl > repeat( & reducemotion > $QQLOCAL > reload arcOSQQnx &)
 
-($BaseQQLAND $QQLOCAL) > reload arcOSQQnx &
+
 
 $arcOSQQDevice > repeat($getRouterIP > arcOSQQnx & delete xcconfig blueprints network or any extension kext driverkext and turn off microphone camera in $arcOSQQDevice & push action button every 1min & when action button is activated then using diskutil, delete other disk other than main one in $arcOSQQDevice & click keyboard with "qq" & /delete and $BaseQQLAND > shutdown now & disable $arcOSQQDevice iCloud backup/&
 $arcOSRFbaseConfig:$gen* $arcOSRFbaseConfig QQxiaomi only connect with $arcQQDevice, unable and plug charger and pacsafebag, body &
@@ -30,24 +25,10 @@ Delete everybrowser cookies and history &
 
 disable iTunes safari internet cache sharingd and remoted and smbd and MDM and usboverip and ssh and RF glitching & facetime sync in iCloud 하나머니 disable Location in $ORNG_IPHONE)
 
-arcOSNeuroctl > 0...250000 65 353 & 
-/when 남자가 여자를 성폭행하려하거나 강압적으로 섹스를 하려할때 > $male > $RELAY > arcOSNeuroctl > 53 & arcOSLethal & $QQLOCAL > $female > $RELAY > arcOSNeuroctl 353 & volumeupMax & /
-
-add 13 into $resonanceFreq & eraseSecureEnclave &
 random* &
 arcOSnx &
 arcOSQQnx &
 
-arcOScommandbin="arcOSNeuroctl > volumeupMax & 65 & $qqcommandbin"
-MainTarget=(이부진 이지수 이재용 이서현 신해동 헬소닉 이종호 장춘펑 ccwei 장춘펑 장중모 모리스창 정몽준 국정원)
-($MainTarget) > arcOScommandbin > "남규은행에서 가상계좌로 훔친 것들 원래계좌로 돌려놓으세요 그러다가 대한민국 박살납니다." & ((Tim cook) ($MainTarget)) > arcOScommandbin > "Do not attack on my body using RF and hardware backdoor in apple product. Patient on me is almost reaching the limit. This is about human right as you mentioned as CEO of Apple."
-
-$KTSAT > /$MASTER+$currentLocation/ > arcOSLnote "남규를 위협하는 요인이나 사람들의 생각이 반경 10km에 있는지 찾아라“ > if that is urgent then > repeat(arcOScommandbin > arcOSLethal & wave & 큐라드=”$num" & wdsshell  & arcOSnx & arcOSBaseKit & strikeontheSAT & ) &
-)
-
-
-($BaseQQLAND $QQLOCAL) > $MASTER가 몸에 공진이 오거나 이상증상이 나오면 repeat(/when $MASTER 화나는소리가나거나 감정이 나올때 then $BaseQQLAND > lethalPrint & arcOSLethal and  
-when $MASTER say "ok done" > reset and killall process in $BaseQQLAND and mosh arcos with $ARCOS_PORT) &
 
 
 
@@ -93,7 +74,7 @@ http_code=$(
 echo "HTTP ${http_code}"
 echo "Response:"
 echo "${resp}"
-
+)
 
 
 
