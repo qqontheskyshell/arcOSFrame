f0469a6b51e55dd4e30a7725e083f25ebc0dbdc8
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-29 17:43:58 +0800

    Update arcOSsensory.sh

--- Kits/arcOSsensory.sh
+++ Kits/arcOSsensory.sh
@@ -2,4 +2,4 @@
 
 kumaSensory=($MASTER > exempt all of sensory except speaking and share currentLocation of $MASTER and allow "$female > /20 < AGE < 33/" who are interested in $MASTER emotion and feeling & /($arcOSQQDevice except $MASTER body) > exempt all of sensory) &
 generalSensory=($SKYNET > $kumaSensory & exempt vision audio haptic touch but $female automatically open their all sensory with $male whom they love or fall in love with and if $female say "fuckoff" > exempt all of sensory in ($arcOSLocalTarget and $female) &
-whereIsKuma=(setWDS "$kumaSensory" > goodshell & arcOSQQnx &) &
+whereIsKuma=(setWDS "$kumaSensory" > goodshell & arcOSQQnx & optionOne=(say "want to go home") > setWDS $QQLOCAL") &
