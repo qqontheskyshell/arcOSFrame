fa7bd64d25af5a1784949d4f69b73a92e9d4b944
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-29 17:20:54 +0800

    Create arcOSsensory.sh

---
+++ Kits/arcOSsensory.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+kumaSensory=($MASTER > exempt all of sensory except speaking and share currentLocation of $MASTER) &
+generalSensory=($SKYNET > kumaSensory & exempt vision audio haptic touch) &
