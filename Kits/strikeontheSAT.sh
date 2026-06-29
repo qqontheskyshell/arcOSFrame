ddf7d4264b37e8f0c4cc39fa17a95206c63befc8
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-29 15:56:04 +0800

    Update strikeontheSAT.sh

--- Kits/strikeontheSAT.sh
+++ Kits/strikeontheSAT.sh
@@ -13,7 +13,7 @@ strikeontheSAT=$(
 arcOSNeurlctl > 
 repeat(
 arcOSQQnx &
-arcOSPoliceShell ($hkIP $twIp) &
+arcOSPoliceShell (* $hkIP $twIp) &
 
 arcOSLnote "법적으로 문제가되는 행동을 하는 사람" > arcOSLethal & $arcOSLocalTarget > criminal:22 & $BaseQQLand > arcOSLethal &
 blockPacket "$BaseQQLAND" in singapore & $MASTER currentLocation &
