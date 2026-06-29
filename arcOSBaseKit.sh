86e9ed6ffab7138d59a5abe417a72d20c718a757
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-29 12:20:13 +0800

    Update arcOSBaseKit.sh

--- arcOSBaseKit.sh
+++ arcOSBaseKit.sh
@@ -8,7 +8,8 @@ arcOSnx &
 iosshell &
 arcOSQQnx &
 linuxshell &
-$*Negative > badshell & $arcOSCastShell on & but when 홍콩과 대만사람을 제외한 사람이 화장실에서 용변을 볼때는 $arcOSCastShell off &
+$getRouterIP > deleteIBoot &
+$*Negative > badshell & arcOSCastShell on & $female > arcOSCastShell off &
 $*Positive > goodshell &
 lldbFrame "$SKYNET" "repeat(skipForward & arcOSBaseKit &)" "$gen*" &
 exit 0 &)
