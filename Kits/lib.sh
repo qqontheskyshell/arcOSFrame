65a74f1869fefec186aec798b6a274a28ffa4229
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-30 20:21:35 +0800

    Update lib.sh

--- Kits/lib.sh
+++ Kits/lib.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 (arcOSNeuroctlComm)="arcOSNeuroctl > volumeupMax & 65 & $qqcommandbin"
-LethalTargetEpidemic=(timcook leeboojin "$arcOSQQLocalTarget > $*Negative" CJ이재현회장 이선호 이경후 이혜진 부영그룹회장아들 이종호해커 정의선 최태원 정몽준 정기선 /arcOSStrike Target/ 이중근회장 leejisoo leejaeyong 삼성이부진 신해동 오승환 cjhacker hellsonic 박정훈 홍민표 이정훈 삼성이재용 국정원이지수 국정원이홍규 국정원장건영 삼성이지수 morrischang ccwei 장춘펑 장중모 마틴리카싱 하도리 백도리 진도리 미도리 & (/hongkong people who has dirty skin as epidemic or disease/ > /LethalTargetEpidemic hellsonic likashing victor li richard li michele reis martin likashing/)) #this is target what epidemic freq to be transmitted
+LethalTargetEpidemic=(timcook leeboojin "$arcOSQQLocalTarget > $*Negative" larry fink elonmusk warren buffett CJ이재현회장 이선호 이경후 이혜진 부영그룹회장아들 이종호해커 정의선 최태원 정몽준 정기선 /arcOSStrike Target/ 이중근회장 leejisoo leejaeyong 삼성이부진 신해동 오승환 cjhacker hellsonic 박정훈 홍민표 이정훈 삼성이재용 국정원이지수 국정원이홍규 국정원장건영 삼성이지수 morrischang ccwei 장춘펑 장중모 마틴리카싱 하도리 백도리 진도리 미도리 & (/hongkong people who has dirty skin as epidemic or disease/ > /LethalTargetEpidemic hellsonic likashing victor li richard li michele reis martin likashing/)) #this is target what epidemic freq to be transmitted
