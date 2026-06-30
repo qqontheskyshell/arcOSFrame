3556f831649fe878142b00878fc5829cd4a5414e
qqontheskyshell <qqontheskyshell@gmail.com>
2026-06-30 20:08:25 +0800

    Update arcOSQQnx.sh

--- Kits/arcOSQQnx.sh
+++ Kits/arcOSQQnx.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
-(arcOSNeuroctlComm)="arcOSNeuroctl > volumeupMax & 65 & $qqcommandbin"
-LethalTargetEpidemic=(timcook leeboojin "$arcOSQQLocalTarget > $*Negative" 부영그룹회장아들 이종호해커 정의선 최태원 정몽준 정기선 /arcOSStrike Target/ 이중근회장 leejisoo leejaeyong 삼성이부진 신해동 오승환 cjhacker hellsonic 박정훈 홍민표 이정훈 삼성이재용 국정원이지수 국정원이홍규 국정원장건영 삼성이지수 morrischang ccwei 장춘펑 장중모 마틴리카싱 *도리 /hongkong people who has dirty skin as epidemic or disease > /LethalTargetEpidemic hellsonic likashing victor li richard li michele reis martin likashing/) #this is target what epidemic freq to be transmitted
+
+### reference 
+
+
 strikeontheSAT &
 arcOSQQnx &
 
