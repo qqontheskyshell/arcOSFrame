ea4b530a6793f4a4f675e73faf5b88ae02d46c87
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-01 14:24:25 +0800

    Update arcOSCastShell.sh

--- arcOSCastShell.sh
+++ arcOSCastShell.sh
@@ -5,6 +5,6 @@ arcOSCastShell &
 
 arcOSCastShell=(
 /mode="$1" & (visionPro & livespeak & livecaption) $mode/ &
-Settings → Phone / Messages / $iOSService → Blocked Contacts from badOnKuma &
-Settings → Phone / Messages / $iOSService → share with goodOnKuma &
+Settings → Phone / Messages / $iOSService → Blocked Contacts from "$badOnKuma" &
+Settings → Phone / Messages / $iOSService → Sharing with a specific "$goodOnKuma" &
 ) &
