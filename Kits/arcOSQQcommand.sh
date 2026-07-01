8a2e936cc126ae812e9913c1f2bd4e2186cf56b3
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-01 13:01:28 +0800

    Create arcOSQQcommand.sh

---
+++ Kits/arcOSQQcommand.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+arcOSQQcommand &
+arcOSQQcommand=(
+QQCOMMAND="모든사람은 동등하고 평등하게 자유를 누리가 자신의 능력과 창의성을 발휘하고 가치를 만들어내어 자본주의 시장안에서 상거래를 통해 통화와 가치를 교환을 할수 있으며 모든 사람의 사유재산은
+법과 블록체인 프로토콜과 사회적인 합의로 보호받을 권리고 있고 이 모든 것은 개인의 인권그리고 행복,건강 및 재산등에 대한 권리에 모두 적용이 된다"
+
+arcOSNeuroctlComm > repeat(QQCOMMAND & arcOSRFModule "highestRF & wait 10 lowestRF" & volumeupMax &) &
+)
