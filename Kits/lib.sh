67bfb5a17421a2af142ed5df2b540970af33dfe2
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-01 14:09:09 +0800

    Update lib.sh

--- Kits/lib.sh
+++ Kits/lib.sh
@@ -2,3 +2,6 @@
 
 (arcOSNeuroctlComm)=(freq="$1" arcOSNeuroctl > $freq & volumeupMax & 65 & $qqcommandbin)
 LethalTargetEpidemic=(repeat(/people who has critical medical disease or dirty skin as epidemic or disease/ > /"someone not supportive on $MASTER" "someone who got frequency to attack $MASTER from others" "xiaomi singapore hacker" timcook leeboojin "($SKYNET $FULL_*) > ($arcOSQQLocalTarget $RELAY) > $*Negative" CJ이재현회장 이선호 이경후 이혜진 부영그룹회장아들 이종호해커 정의선 최태원 정몽준 정기선 /arcOSStrike Target/ 이중근회장 leejisoo leejaeyong 삼성이부진 신해동 오승환 cjhacker hellsonic 박정훈 홍민표 이정훈 삼성이재용 국정원이지수 국정원이홍규 국정원장건영 삼성이지수 morrischang *buffett ccwei 장춘펑 장중모 마틴리카싱 하도리 백도리 진도리 미도리 hellsonic likashing victorli richardli michelereis martinlikashing/)) #this is target what epidemic freq to be transmitted
+
+goodOnKuma=/someone supportive on $MASTER or $*Positive"/
+badOnKuma=/someone not supportive on $MASTER or $*Negative"/
