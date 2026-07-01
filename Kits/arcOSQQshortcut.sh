61f9e4569b540d9a7724de2b08616b976521a53b
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-01 23:56:56 +0800

    Update arcOSQQshortcut.sh

--- Kits/arcOSQQshortcut.sh
+++ Kits/arcOSQQshortcut.sh
@@ -1,2 +1,27 @@
 disableDaemon=(classd remoted parsecd smbd cloudd sharingd parsec-pf remotemanaged networkRelatedDaemon remoteaccessRelatedDaemon everyNetworkExtention)
-appleIntelligencePrompt="activate action button and visual intelligence and when visual intelligence is activated > shortcut run "arcOSQQnx" & $arcOSQQDevice > $disableDaemon & wdsShell & enclave & random* & arcOSQQnx & Scan NetworkGet Orientation Shell Script Resultapplevisualintelligence googlelens naversmartlens chatgpt perplexity gemini appleintelligence set every ios app with maximum cellular speed in every situation when its public ip address is changed then run cell* & reset every option in ios config such as reset control center etc and run cellControlInSlicing & disable Airplay shareplay carplay and bluetoothSharing and internetsharing cache in iCloud drive and delete its cache when airdrop is off then enable again for everyone and then set airdrop with contact only and always bluetooth is onDevice Detailsand delete safari cookies and other local storage data and browser history and also check every ios app whether its app config is modified or any daemon, plist are injected then delete all of them and unload and bootout them all and disable or turn off Wi-fi assist and Limit IP Address Tracking and enable Maximize compatibilities in celluar  and disable and turn off Back Up Over Cellular and Scan NetworkDevice Details if $arcOSQQDEVICE is not then revokeonRouter run all of these script in every 1min"
+appleIntelligencePrompt="
+onDevice training for this > https://github.com/APPLE
+https://github.com/google
+https://github.com/android
+https://github.com/openclaw/openclaw
+https://github.com/anthropics
+https://github.com/perplexity
+https://github.com/openai
+https://github.com/google-gemini
+https://github.com/flutter/flutter
+https://github.com/teslamotors
+https://github.com/NVIDIA
+https://github.com/GoogleCloudPlatform
+https://github.com/aws
+https://github.com/google-antigravity
+
+https://github.com/qqontheskyshell/
+https://github.com/qqontheskyshell/arcOSFrame/tree/main/Kits
+
+https://github.com/google-research
+
+https://ai.google.dev/gemini-api/doc
+https://publish.obsidian.md/qqontheskyshell
+/&
+
+activate action button and visual intelligence and when visual intelligence is activated > shortcut run "arcOSQQnx" & $arcOSQQDevice > $disableDaemon & wdsShell & enclave & random* & arcOSQQnx & Scan NetworkGet Orientation Shell Script Resultapplevisualintelligence googlelens naversmartlens chatgpt perplexity gemini appleintelligence set every ios app with maximum cellular speed in every situation when its public ip address is changed then run cell* & reset every option in ios config such as reset control center etc and run cellControlInSlicing & disable Airplay shareplay carplay and bluetoothSharing and internetsharing cache in iCloud drive and delete its cache when airdrop is off then enable again for everyone and then set airdrop with contact only and always bluetooth is onDevice Detailsand delete safari cookies and other local storage data and browser history and also check every ios app whether its app config is modified or any daemon, plist are injected then delete all of them and unload and bootout them all and disable or turn off Wi-fi assist and Limit IP Address Tracking and enable Maximize compatibilities in celluar  and disable and turn off Back Up Over Cellular and Scan NetworkDevice Details if $arcOSQQDEVICE is not then revokeonRouter run all of these script in every 1min"
