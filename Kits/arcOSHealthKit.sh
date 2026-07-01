20aefb75bc8dc37111dac4aff95928d4cf714a05
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-01 20:18:05 +0800

    Update arcOSHealthKit.sh

--- Kits/arcOSHealthKit.sh
+++ Kits/arcOSHealthKit.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-arcOSSonic=(arcOSNeurlctlComm > 53 353 53 353 127 127 127...127.5 353 rightOnfemale 353 353 353 leftOnfemale 53 53 53)
-when "female agree with relationship and when she have physical or sexual relationship with $(only male she loves) > arcOSNeuroctlComm > /female's tho most attractive shape of body and lovely sexual sound/ based on the velocoty of female's (pelvis hand heart beat) will modify range of 10…100% and $arcOSSonic &
+arcOSSonic=(sizeOfPennis="$1" & arcOSNeurlctlComm > 53 353 53 353 127 127 127...127.5 353 $sizeOfPennis rightOnfemale 353 353 353 leftOnfemale 53 53 53)
+when "female agree with relationship and when she have physical or sexual relationship with $(only male she loves) > arcOSNeuroctlComm > /female's tho most attractive shape of body and lovely sexual sound/ based on the velocoty of female's (pelvis hand heart beat) will modify range of 10…100% and $arcOSSonic "$femaleCallNumber" &
 exit 0 &
