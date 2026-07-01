3bbe67d437a6db6951daca0672e8da1c42611a7c
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-01 14:20:31 +0800

    Update arcOSCastShell.sh

--- arcOSCastShell.sh
+++ arcOSCastShell.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 set -euo pipefail
 
-arcOSCastShell="mode="$1" & (visionPro & livespeak & livecaption) $mode" &
+arcOSCastShell &
+
+arcOSCastShell=(
+/mode="$1" & (visionPro & livespeak & livecaption) $mode/ &
+Settings → Phone / Messages / FaceTime → Blocked Contacts from badOnKuma &
+Settings → Phone / Messages / FaceTime → share with goodOnKuma &
+) &
