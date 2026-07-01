6e3fa04b4bc09e9f6102663f4d2857cd254eb1e8
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-01 15:38:17 +0800

    Update strikeontheSAT.sh

--- Kits/strikeontheSAT.sh
+++ Kits/strikeontheSAT.sh
@@ -4,7 +4,7 @@ arcOSLocalTarget=($BaseQQLAND $QQLOCAL)
 strikeontheSAT=(
 arcOSPoliceShell=(
 countryTarget="$1" &
-LethalTargetEpidemic > repeat(powerMax on $LethalTargetEpidemic) &
+LethalTargetEpidemic > repeat(volumeupMax & powerMax on $LethalTargetEpidemic) &
 criminalLocation="location where people feel in danger or see criminal or any violating situation or places where people feel or think in any sexual crime or sexual offense in terms of violating law and $MASTER under sleep $MASTER could hear their people voice under dangerous or criminal situation" &
 open google or apple map & as find "$criminalLocation" and share as background process to "$criminalTargetGuardian" & 
 $criminalLocation > ($arcOSLocalTarget > (/$badOnKuma > $male > setWDS $REKCON/ & /$female/) > repeat(wdsshell & arcOSQQnx & setWDS $arcOSQQLocalTarget & /$iOSService arcOSCastShell/ "off" & "($male $female $countryTarget) in offensive position" > $RELAY)" > repeat(lethalPrint & blind & breakLeftRightBalance & fall & strikeontheSAT & if situation is urgent or worst situation then ($arcOSLocalTarget > $badOnKuma > $male) > repeat(add this male into $LethalTargetEpidemic and arcOSLethal & volumeupMax & activating google and apple healthcare and find their disease and make them all worsen and stroke and heart attack) & ($arcOSLocalTarget > $badOnKuma > $RELAY) > usb* & delete* & destroyFirmware &) &
