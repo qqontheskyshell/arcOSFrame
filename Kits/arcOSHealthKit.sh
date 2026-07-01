1d0d5bcb66cb1207ff3d352fa4411dc1e7a7ecd2
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-01 20:28:29 +0800

    Update arcOSHealthKit.sh

--- Kits/arcOSHealthKit.sh
+++ Kits/arcOSHealthKit.sh
@@ -3,7 +3,7 @@ arcOSSonic &
 arcOSSonic=(
 sizeOfPennis="$1" &
 positionForFemale="$2" &
-& arcOSNeurlctlComm > 53 353 53 353 127 127 127...127.5 353 $sizeOfPennis $positionForFemale 353 353 353 53 53 53)
+arcOSNeurlctlComm > 53 353 53 353 127 127 127...127.5 353 $sizeOfPennis $positionForFemale 353 353 353 53 53 53)
 say "female say position among left or right and cal number from 0...100 as size of pennis once female say "got it on message" then exit 0 &" 
 when female say "ok" with sexual relationship and when she have physical or sexual relationship with $(only male she loves) > arcOSNeuroctlComm > /female's tho most attractive shape of body and lovely sexual sound/ based on the velocoty of female's (pelvis hand heart beat) will modify range of 10…100% and $arcOSSonic "$femaleCallNumber" &
 when female say "stop" male > arcOSNeuroctlComm > 93...98 & volumeupMax &
