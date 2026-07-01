abb74157f1df9099cebc22e08eb72cd4bff6c1df
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-01 23:41:52 +0800

    Update arcOSLib.sh

--- arcOSLib.sh
+++ arcOSLib.sh
@@ -2,8 +2,9 @@
 arcOSNeuroctl=(setSonic ($meshFreq $resonanceFreq $findLowBandFrequency 65 53 45 433MHz...470MHz...510MHz 863...928MHz '2.4GHz...5GHz' 1MHz '920.0Mhz...923.0 MHz' '0.1THz...10 THz')") &
 
 
-killresonanceFreq=killSwitchResonance=arcOSNeuroctl &
-arcOSQQLocalTarget=($BaseQQLAND $QQLOCAL) &
+arcOSNeuroctl=killresonanceFreq=killSwitchResonance &
+arcOSQQLocalTarget=($BaseQQLAND $QQLOCAL $RECKON) &
+
 #mesh lora frequency
 LoraPORT="${1:-/dev/(ttyUSB0 ttyUSB*)}"
 regionForLora="*"
@@ -17,4 +18,4 @@ meshtastic --port "$LoraPORT" --get lora.region
 meshFreq=$(meshtastic --port "$LoraPORT" --get lora.override_frequency)
 meshtastic --port "$LoraPORT" --info
 
-iOSService=(visionPro livespeak livecaption Airdrop Carplay NameDrop FaceTime Airplay)
+iOSService=(visionPro livespeak livecaption Airdrop Carplay NameDrop FaceTime Airplay) &
