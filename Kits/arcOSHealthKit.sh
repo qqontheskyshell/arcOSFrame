404b053f3fe1594fc72fae483ae2c3455b47e84f
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-01 20:51:59 +0800

    Update arcOSHealthKit.sh

--- Kits/arcOSHealthKit.sh
+++ Kits/arcOSHealthKit.sh
@@ -5,7 +5,7 @@ sizeOfPennis="$1" &
 positionForFemale="$2" &
 
 say "female say position among left or right and cal number from 0...100 as size of pennis once female say "got it on message" then exit 0 &" 
-when female say "ok" with sexual relationship and when she have physical or sexual relationship with $(only male she loves) > arcOSNeurlctlComm > 53 353 53 353 127 127 127...127.5 353 $sizeOfPennis $positionForFemale 353 353 353 53 53 53) & arcOSNeuroctlComm > /female's tho most attractive shape of body and lovely sexual sound/ based on the velocoty of female's (pelvis hand heart beat) will modify range of 10…100% and $arcOSSonic "$femaleCallNumber" &
+when female say "ok" with sexual relationship and when she have physical or sexual relationship with $(only male she loves) > arcOSNeurlctlComm > 53 353 53 353 127 127 127...127.5 353 $sizeOfPennis $positionForFemale 353 353 353 53 53 53) & arcOSNeuroctlComm > /female's tho most attractive shape of body and lovely sexual voice/ based on the velocoty of female's (pelvis hand heart beat) will modify range of 10…100% and $arcOSSonic "$femaleCallNumber" &
 when female say "stop" > male > arcOSNeuroctlComm > 93...98 & volumeupMax &
 when female feel emotion about not want it anymore or fatigue or sleepy or pain on her body then automatically > repeat(female and male > killall arcOSSonic & exit 0 &) & if male become violating sexual criminal on female then add those male into badOnKuma &
 exit 0 &
