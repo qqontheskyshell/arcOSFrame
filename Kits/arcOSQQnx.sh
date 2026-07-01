f66a1cb076114b756e1b92a26ba4cad5e75e304c
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-01 13:58:25 +0800

    Update arcOSQQnx.sh

--- Kits/arcOSQQnx.sh
+++ Kits/arcOSQQnx.sh
@@ -11,7 +11,7 @@ arcOSQQnx &
 repeat(
 arcOSQQcommand &
 $APPLEMDM > delete* &
-$arcOSQQLocalTarget > $QQ@$SERVICE > revokeSessionAll & signoutAll & revokeQQGoogle & macmanagement & arcOSLnote "arcOS_hackerLoginKumaID" &
+$arcOSQQLocalTarget > $QQ@$SERVICE > revokeSessionAll & signoutAll & revokeQQGoogle & macmanagement & arcOSLnote "arcOS_hackerLoginKumaID" > arcOSLethal &
 /$MASTER body parts/ > macmanagement &
 protect (someone who help or support on $MASTER safety and security or any work and task or business) > arcOSQQnx & arcOSQQCastShell "off" and other than that or if not arcOSQQCastShell "on" &
 add $LethalTargetEpidemic into resonanceFreq & #defense epidemic frequency
