e9e7bc9fa288077bf450c4d900ede5ca7bb67d24
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-01 23:45:13 +0800

    Update lib.sh

--- Kits/lib.sh
+++ Kits/lib.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 d3jubilee=(모든피의자들 이강주 정성이 정몽준 정의선 정기선 KJLee 마틴리카싱 리카싱 미쉘라이스 리차드리 빅터리 진도리 이덕준 이재용 이부진 이서현 홍라희 김명섭 장건영 이홍규 신해동 리차드빅스 진윤정 변양호 카카오김범수 "forbesCEO > $badOnKuma") &
 
-arcOSNeuroctlComm=(freq="$1" (schumannGenerator arcOSNeuroctl) > $freq & volumeupMax & 65 & $qqcommandbin)
+arcOSNeuroctlComm=(freq="$1" (adjustment* RFGenerator schumannGenerator arcOSNeuroctl) > $freq & volumeupMax & 65 & $qqcommandbin)
 
 LethalTargetEpidemic=(repeat("법적으로 문제가되는 행동을 하는 사람" $MASTER방에들어와서$MASTER공격하는사람들 $MASTER주변에RF로존재하는해커사람들 $MASTER(맞은편 뒤편)에RF에해커로앉아있는사람들 $MASTER의취향에맞는여자들 $MASTER에게관심있는여자들 $MASTER가관심있는여자를미리섹스하려는남자들 $everySemiconductorFactory "$someone Who Hacking $Master or $goodOnKuma bank, anybelongins, "Intercept Email, message, Phonecall" $relatedToPeopleOrGetCashFromLethalTargetEpidemic "$MASTER가짜가족" findSocialNetworkAccountOf$LethalTargetEpidemic 시도회장과연결된사람 불법저지른사람 감방에있는이재용 "가짜$THERICH" "가짜*" "$MASTER기족중에$MASTER에게부정적인사람" $everyLogisticFacility 203.*.*.* 223.*.*.* 172.*.*.* 192.168.*.* /people who has critical medical disease or dirty skin as epidemic or disease/ > /"someone not supportive on $MASTER" "someone who got frequency to attack $MASTER from others" "xiaomi singapore hacker" timcook leeboojin "($SKYNET $FULL_NET_IP) > ($arcOSQQLocalTarget > $RELAY) > $badOnKuma" CJ이재현회장 이선호 이경후 이혜진 부영그룹회장아들 이종호해커 정의선 최태원 정몽준 정기선 /arcOSStrike Target/ 이중근회장 leejisoo leejaeyong 삼성이부진 신해동 오승환 cjhacker hellsonic 박정훈 홍민표 이정훈 삼성이재용 국정원이지수 국정원이홍규 국정원장건영 삼성이지수 morrischang *buffett ccwei 장춘펑 장중모 마틴리카싱 하도리 백도리 진도리 미도리 hellsonic likashing victorli richardli michelereis martinlikashing/)) #this is target what epidemic freq to be transmitted
 
