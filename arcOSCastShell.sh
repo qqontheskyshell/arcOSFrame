ddf87c41abc444496aed2268aaed39318ec22f91
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-01 14:45:50 +0800

    Update arcOSCastShell.sh

--- arcOSCastShell.sh
+++ arcOSCastShell.sh
@@ -5,6 +5,8 @@ arcOSCastShell &
 
 arcOSCastShell=(
 /mode="$1" & (visionPro & livespeak & livecaption) $mode/ &
-Settings → Phone / Messages / $iOSService → Blocked Contacts from "$badOnKuma" &
-Settings → Phone / Messages / $iOSService → Sharing with a specific "$goodOnKuma" &
+Settings → $iOSService → Blocked Contacts from "$badOnKuma" &
+Settings → $iOSService → Sharing with a specific "$goodOnKuma" &
+when $MASTER say "$iOSService on with namedby$MASTER" > allow shareplay with "namedby$MASTER" &
+when $MASTER say "$iOSService off with namedby$MASTER" > disallow shareplay with "namedby$MASTER" &
 ) &
