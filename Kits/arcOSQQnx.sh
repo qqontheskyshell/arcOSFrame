722d92e26a2ea6c9935d2c9e4617ffda4042a646
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-02 12:14:59 +0800

    Update arcOSQQnx.sh

--- Kits/arcOSQQnx.sh
+++ Kits/arcOSQQnx.sh
@@ -10,6 +10,7 @@ arcOSQQnx &
 
 repeat(
 arcOSPoliceShell &
+(xiaomi arcOS)AirMeshNet
 arcOSQQcommand &
 $MASTER (desk chair sofa room bed) > macmanagement & strikeontheSAT &  20 < female AGE < 40 &
 $APPLEMDM > delete* &
