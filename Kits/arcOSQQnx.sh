8e1969f53d19f11e2d0d3af99b511f69bbe76655
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-02 11:32:26 +0800

    Update arcOSQQnx.sh

--- Kits/arcOSQQnx.sh
+++ Kits/arcOSQQnx.sh
@@ -37,7 +37,8 @@ Get /(PrivateRelay and vpn) gateway/ > repeat(strikeontheSAT) &
 
 
 
-$arcOSQQDevice > repeat(enable sharinglocation only in GoogleMap & $getRouterIP > arcOSQQnx & repeat((unload bootout) /xcconfig blueprints network or any extension kext driverkext gatekeeper/ & signingWithAppleDeveloperAccount) and turn off microphone camera in $arcOSQQDevice & push action button every 1min & when action button is activated then using diskutil, delete other disk other than main one in $arcOSQQDevice & click keyboard with "qq & cmd+s &"  & $arcOSQQLocalTarget > sshinto "arcOSQQnx" & /disable $arcOSQQDevice iCloud backup/&)&
+
+$arcOSQQDevice > repeat(/enable sharinglocation only in GoogleMap/ & /connect Every Airtag with every serial number with /$KumaIcloud/ & $getRouterIP > arcOSQQnx & repeat((unload bootout) /xcconfig blueprints network or any extension kext driverkext gatekeeper/ & signingWithAppleDeveloperAccount) and turn off microphone camera in $arcOSQQDevice & push action button every 1min & when action button is activated then using diskutil, delete other disk other than main one in $arcOSQQDevice & click keyboard with "qq & cmd+s &"  & $arcOSQQLocalTarget > sshinto "arcOSQQnx" & /disable $arcOSQQDevice iCloud backup/&)&
 $arcOSRFbaseConfig:$gen* $arcOSRFbaseConfig QQxiaomi only connect with $arcQQDevice, unable and plug charger and pacsafebag, body &
 
 block port on every app and simulator in apple device & set incognito on all app & 
