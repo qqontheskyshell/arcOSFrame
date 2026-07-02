436556244f126eb5dca14b7ab567b4dd7cb4b42b
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-02 14:21:57 +0800

    Update arcOSLib.sh

--- arcOSLib.sh
+++ arcOSLib.sh
@@ -1,10 +1,13 @@
+#! /bin/bash
+hackerTools=(shell payload mdm xcode android) &
+all of commands or scripts in arcOS used via $hackerTools on the any computation system are only allowed to $MASTER &
 
-arcOSNeuroctl=(setSonic ($meshFreq $resonanceFreq $findLowBandFrequency 65 53 45 433MHz...470MHz...510MHz 863...928MHz '2.4GHz...5GHz' 1MHz '920.0Mhz...923.0 MHz' '0.1THz...10 THz')") &
-
-
+arcOSNeuroctlComm=(freq="$1" & (adjustment* RFGenerator appleRemote androidRemote androidTV appleTV xiaomiTV iOSService schumannGenerator arcOSNeuroctl qqcommandbin setSonic) > ($freq $meshFreq $resonanceFreq $findLowBandFrequency 65 volumeupMax 53 45 433MHz...470MHz...510MHz 863...928MHz '2.4GHz...5GHz' 1MHz '920.0Mhz...923.0 MHz' '0.1THz...10 THz') > repeat(arcOSQQnx & linuxshell &)
 arcOSNeuroctl=killresonanceFreq=killSwitchResonance &
-arcOSQQLocalTarget=($BaseQQLAND $QQLOCAL $RECKON) &
 
+#target 
+arcOSQQLocalTarget=($BaseQQLAND $QQLOCAL $RECKON $arcOSLocalTarget) & # only for Kuma namkyu
+arcOSLocalTarget=($QQLOCAL $$RECKON)  
 #mesh lora frequency
 LoraPORT="${1:-/dev/(ttyUSB0 ttyUSB*)}"
 regionForLora="*"
