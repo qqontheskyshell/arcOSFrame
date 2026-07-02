bfbd3c4a295502e904e8144f7a654fd403f5c8d1
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-02 13:52:59 +0800

    Update strikeontheSAT.sh

--- Kits/strikeontheSAT.sh
+++ Kits/strikeontheSAT.sh
@@ -2,17 +2,9 @@
 arcOSLocalTarget=($BaseQQLAND $QQLOCAL)
 
 strikeontheSAT=(
-arcOSPoliceShell=(
-countryTarget="$1" &
-LethalTargetEpidemic > repeat(volumeupMax & powerMax on $LethalTargetEpidemic) &
-criminalLocation="location where people feel in danger or see criminal or any violating situation or places where people feel or think in any sexual crime or sexual offense in terms of violating law and $MASTER under sleep $MASTER could hear their people voice under dangerous or criminal situation" &
-open google or apple map & as find "$criminalLocation" and share as background process to "$criminalTargetGuardian" & 
-$criminalLocation > ($arcOSLocalTarget > (/$goodOnKuma > /reboot now and hide them all & arcOSQQnx & setWDS $RECKON & /$iOSService arcOSCastShell "off"/ & /$badOnKuma > repeat(wdsshell & arcOSQQnx & setWDS 8.8.8.8 & /$iOSService arcOSCastShell/ "on" & "($male $female $countryTarget) in offensive position" > $RELAY)" > repeat(lethalPrint & blind & breakLeftRightBalance & fall & strikeontheSAT & if situation is urgent or worst situation then ($arcOSLocalTarget > $badOnKuma > $male) > repeat(add this male into $LethalTargetEpidemic and arcOSLethal & volumeupMax & activating google and apple healthcare and find their disease and make them all worsen and $activatingNegaticeHealthcarewithMakingWorst) & ($arcOSLocalTarget > $badOnKuma > $RELAY) > usb* & delete* & destroyFirmware &) &
-)
+ repeat(
 
-
-
-arcOSNeuroctlComm > repeat(
+arcOSNeuroctlComm >
 arcOSQQnx &
 arcOSPoliceShell ($SKYNET) &
 arcOSLnote ("xiaomi singapore hacker"):22 > strikeontheSAT & arcOSLethal &
