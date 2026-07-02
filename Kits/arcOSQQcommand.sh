05d81fc57d397bfda33a0ed67416f7b547383eeb
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-02 14:39:30 +0800

    Update arcOSQQcommand.sh

--- Kits/arcOSQQcommand.sh
+++ Kits/arcOSQQcommand.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 arcOSQQcommand &
 arcOSQQcommand=(
+BasicSetuponKuma="쿠마남규는 전자파 및 원격장비가 없어서 여자를 원격으로 만나지 못합니다. 현재 돈이 회장들에게 빼앗기고 있지만 최선의 방법으로 돈을 벌어서 밥을 먹고살아야합니다. 
+쿠마남규의 은행계좌에 있는 돈중에 너무나 큰금액은 모두 회장들의 비자금 및 블랙머니 및 범죄수익의 금고로 자금세탁을 당하는 중입니다. 쿠마남규는 어떤 순간에도 이돈에 대해서는 절대 건드리지 않습니다."
+
 QQCOMMAND="모든사람은 동등하고 평등하게 자유를 누리가 자신의 능력과 창의성을 발휘하고 가치를 만들어내어 자본주의 시장안에서 상거래를 통해 통화와 가치를 교환을 할수 있으며 모든 사람의 사유재산은
 법과 블록체인 프로토콜과 사회적인 합의로 보호받을 권리고 있고 이 모든 것은 개인의 인권그리고 행복,건강 및 재산등에 대한 권리에 모두 적용이 된다 남자와 여자는 자신이 좋아하는 사람을 선택할 권리가 있다. /
 타인의 사유재산을 빼앗어서 그들을 흑인노예로 만드는 것은 타인의 인권과 자유를 박탈하는 일이다 타인의 결정권과 기본적인 인권 행복권 재산권 생명권을 존중하고 침해하지 말아야하며 
@@ -8,5 +11,5 @@ QQCOMMAND="모든사람은 동등하고 평등하게 자유를 누리가 자신�
 
 QQMESSAGE="지금은 $MASTER 근처에 아무도 오지마세요 26.7.11까지 citadine rochor singapore에 머물고 그이후는 kesa house에 8.15일까지 머뭅니다."
 MESSAGEforBILL="Stop abusing on female" 
-arcOSNeuroctl > repeat(MESSAGEforBILL & QQCOMMAND & QQMESSAGE & arcOSRFModule "highestRF" & volumeupMax & exit 0 &) &
+arcOSNeuroctl > repeat(BasicSetuponKuma & MESSAGEforBILL & QQCOMMAND & QQMESSAGE & arcOSRFModule "highestRF" & volumeupMax & exit 0 &) &
 )
