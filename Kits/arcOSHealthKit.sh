33062a5332ab85c0b13c89ea7a2a51564d27ff36
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-02 14:22:42 +0800

    Update arcOSHealthKit.sh

--- Kits/arcOSHealthKit.sh
+++ Kits/arcOSHealthKit.sh
@@ -6,13 +6,13 @@ positionForFemale="$2" &
 relationTarget=$(only male she loves)
 say "female say position among left or right and cal number from 0...100 as size of pennis once female say "got it on message" then exit 0 &" 
 when female say "ok" with sexual relationship and when she have physical or sexual relationship with $relationTarget
-$relationTarget > arcOSNeurlctlComm > 53 353 53 353 127 127 127...127.5 353 $sizeOfPennis $positionForFemale 353 353 353 53 53 53) & arcOSNeuroctlComm > /female's tho most attractive shape of body and lovely sexual voice/ based on the velocoty of female's (pelvis hand heart beat) will modify range of 10…100% and $arcOSSonic "$femaleCallNumber" &
+$relationTarget > arcOSNeurlctlComm > 53 353 53 353 127 127 127...127.5 353 $sizeOfPennis $positionForFemale 353 353 353 53 53 53) & arcOSNeuroctl > /female's tho most attractive shape of body and lovely sexual voice/ based on the velocoty of female's (pelvis hand heart beat) will modify range of 10…100% and $arcOSSonic "$femaleCallNumber" &
 
 criticalInfo=(bankaccount password financialInformation)
-female > arcOSNeuroctlComm "male sound except $criticalInfo" &
-male > arcOSNeuroctlComm "female sound except $criticalInfo" &
+female > arcOSNeuroctl "male sound except $criticalInfo" &
+male > arcOSNeuroctl "female sound except $criticalInfo" &
 
-when female say "stop" > male > arcOSNeuroctlComm > 93...98 & volumeupMax &
+when female say "stop" > male > arcOSNeuroctl > 93...98 & volumeupMax &
 when female feel emotion about not want it anymore or fatigue or sleepy or pain on her body then automatically > repeat(female and male > reload arcOSQQnx & killall arcOSSonic & exit 0 &) & if male become violating sexual criminal on female then add those male into "$badOnKuma" &
 
 
