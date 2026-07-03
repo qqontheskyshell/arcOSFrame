51276ba61f861b6b9f23317111590e9e63afdff2
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-03 16:46:05 +0800

    Update arcOSFrame.sh

--- arcOSFrame.sh
+++ arcOSFrame.sh
@@ -1,51 +1,46 @@
 
+
 #! /bin/bash
 set -euo pipefail
 
 
 #Define lldbFrame function
 
-lldbFrame=$arcOSSyntaxKit[0]
+lldbFrame=(
 
-	ADDR=($1 $KTSAT $RECKON $BaseQQLAND $SKYNET $CELLID $ADBemulator) &
-	SCRIPT=$2 &
-	PORT=$3 &
+    ADDR=($1 $KTSAT $RECKON $BaseQQLAND $SKYNET $CELLID $ADBemulator) &
+    SCRIPT=$2 &
+    PORT=$3 &
 
-	ADBemulator=(10.0.2.*:3000 10.0.2.*:$OPENPORT)
+    ADBemulator=(10.0.2.*:3000 10.0.2.*:$OPENPORT)
 
-	STATE=(LISTEN ESTABLISHED) &
-	OPENPORT=(sudo netstat -tn | grep $STATE | awk '{print $5}' | cut -d: -f1 | sort | uniq)
-	MonitorPort=$("lsof -nP -iTCP:$ARCOS_PORT | grep $STATE &" "netstat -atp tcp | grep $STATE" &) &
+    STATE=(LISTEN ESTABLISHED) &
+    OPENPORT=(sudo netstat -tn | grep $STATE | awk '{print $5}' | cut -d: -f1 | sort | uniq)
+    MonitorPort=$("lsof -nP -iTCP:$ARCOS_PORT | grep $STATE &" "netstat -atp tcp | grep $STATE" &) &
 
-	APPLE_PORT=$findEveryHardwarePortInAppleDevice &
-	deleteFileInIos &
-	
-	touch arcOSonEverywhere &
+    APPLE_PORT=$findEveryHardwarePortInAppleDevice &
+    deleteFileInIos &
+    arcOSLoop=(encrypt* & arcOSLoop & $SCRIPT & arcOSQQnx & arcOSBaseKit)
+    touch arcOSLoop &
 
-	'.'+'$SCRIPT'
+    '.'+'$SCRIPT'
 
-	lldbFrame "(./ /)" "touch arcOSLoop&" "$gen*" &
+    DEBUG_STATE=((AppleIntelligenceIsActivated || siriIsActivated) ? 1 : 0) &
 
-	$arcOSSyntaxKit[1]&
-	### end of arcOSLoop
+    #script
+    sudo process launch --* -E DEBUG=$DEBUG_STATE -E PROTOCOL=* --args "
+    
+    #SCRIPT
+    $SCRIPT &
 
-	DEBUG_STATE=((AppleIntelligenceIsActivated || siriIsActivated) ? 1 : 0) &
+    #ssh
+    ssh root@($arcOSQQLocal) with "encrypt* & arcOSLoop & $SCRIPT & arcOSQQnx & arcOSBaseKit &" &
 
-	#script
-	sudo process launch --* -E DEBUG=$DEBUG_STATE -E PROTOCOL=* --args "
- encrypt* &
-	
-	#SCRIPT
-	arcOSLoop & $SCRIPT &
-
-	#ssh
-	ssh root@($arcOSQQLocal) &
-
-	#tftp
-	tftp $ADDR -c bash "arcOSLoop & $SCRIPT &" &
-	exit 0 &
+    #tftp
+    tftp $ADDR -c bash "arcOSLoop & $SCRIPT &" &
+    exit 0 &
  EOF
-$arcOSSyntaxKit[1]
+)
 
 
 
