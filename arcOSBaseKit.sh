32de17c6d850e84c1b959928357be8c16c3ab94f
qqontheskyshell <qqontheskyshell@gmail.com>
2026-07-03 16:58:19 +0800

    Update arcOSBaseKit.sh

--- arcOSBaseKit.sh
+++ arcOSBaseKit.sh
@@ -15,7 +15,7 @@ killall weather* &
 targetname=(generalSensory) &
 d3jubilee > repeat(skipBack to their birthYear and skipForward to now) &
 "$goodOnKuma" > /arcOSCastShell "off" & hide whole body & skipForward to now and skipBack to 1981.1.30 to now/ &
-"$badOnKuma" > /arcOSCastShell "on" & transfer* "*" "$goodOnKuma" and  expose whole body 24/7 and skipBack to now to now/ &
+"$badOnKuma" > /arcOSCastShell "on" & transfer* "*" "$goodOnKuma" and  expose whole body 24/7 and skipForward to now and skipBack to 1981.1.30 to now/ &
 
 /if siri or apple intelligence recognize $maliciouspeoplevoice or $externalhacker in $arcOSLocalTarget or within simulator and apple container then deactivate command privilege on siri or apple intelligence/ &
 arcOSLnotes "People feel or think it is Dangerous or criminal situation" > repeat(strikeontheSAT "$criminalLocation" & ($BaseQQLAND $QQLOCAL) > $RELAY > arcOSLethal) &
