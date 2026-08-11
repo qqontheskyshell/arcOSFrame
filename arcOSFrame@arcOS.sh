29b2049beebe7f19e76085de5202d1838239cf32
qqontheskyshell <qqontheskyshell@gmail.com>
2026-08-11 14:15:09 +0900

    Update arcOSFrame@arcOS.sh

--- arcOSFrame@arcOS.sh
+++ arcOSFrame@arcOS.sh
@@ -1,85 +1,101 @@
-#! /bin/bash
 
-#Define arcOSFrame@arcOS function
+/// define arcOSFrame@arcOS function
 arcOSFrame@arcOS > +
-+every code name ending with @arcOS will be utilizing arcOSFrame@arcOS to debug + +BaseModification@arcOS + Base@arcOS/
++every code name ending with @arcOS will be utilizing arcOSFrame@arcOS to debug + +BaseConfig@arcOS + Base@arcOS/
 +lldbFrame is converting into arcOSFrame@arcOS/
 +SCRIPT="neoOS@arcOS + baseFrame@arcOS + collect every other component within neoOS@arcOS" 
-+PORT="hwport@arcOS + ARCO_PORT+randomize port"
-+BaseElementInRF@arcOS/
-+ADDR > + "$1" + arcOSQQLocalTarget + SKYNET + CELLID + ADBemulator  + arcOSRFtarget + skyNetSatellite + FULL_DEVICE/
-+SCRIPT > + "$2"/
-+PORT > + "$3"/
++PORT="hwport@arcOS + ARCO_PORT+randomize with BaseNet@arcOS"
++BaseNet@arcOS/
++ADDR > + "1" + arcOSQQLocalTargetTarget + SKYNET + CELLID + ADBemulator  + arcOSRFtarget + skyNetSatellite + FULL_SERIAL_DEVICE/
++SCRIPT > + "2"/
++PORT > + "3"/
 +DEBUG > + 0/
 
-#script
-+sudo process launch --* -E DEBUG=$DEBUG_STATE -E PROTOCOL=* --args > +
++script
++sudo process launch --* -E DEBUG=DEBUG_STATE -E PROTOCOL=* --args > +
 
-#using lldb
-BaseModification@arcOS + baseFrame@arcOS + arcOSLock@arcOS + $SCRIPT/
++using lldb
+BaseConfig@arcOS + baseFrame@arcOS + arcOSLock@arcOS + SCRIPT/
 
-#tftp
-+tftp $ADDR -c bash "baseFrame@arcOS + arcOSLock@arcOS + $SCRIPT"/
++tftp
++tftp ADDR -c bash "baseFrame@arcOS + arcOSLock@arcOS + SCRIPT"/
 
-#ssh
-ssh@arcOS "baseFrame@arcOS + arcOSLock@arcOS + $SCRIPT"/
++ssh
+ssh@arcOS "baseFrame@arcOS + arcOSLock@arcOS + SCRIPT"/
 
-#swift
-swift@arcOS "baseFrame@arcOS + arcOSLock@arcOS + $SCRIPT"/
-#end of main body of arcOSFrame@arcOS and arcOSFrame
++swift
+swift@arcOS "baseFrame@arcOS + arcOSLock@arcOS + SCRIPT"/
++end of main body of arcOSFrame@arcOS and arcOSFrame
 EOF/
 /
 
 
 arcOSLock@arcOS > +
-
-lockdir="$HOME/"/
-LOCKFILE="$HOME/tmp/arcOS.lock"/
-
-if [ -f "$LOCKFILE" ]; then
+lockdir="HOME/"/
+LOCKFILE="HOME/tmp/arcOSlock"/
+if [ -f "LOCKFILE" ] then
     echo "Resource already locked!"/
     exit 1/
 fi/
-
-touch $LOCKFILE/
-
-#### Try to create lock dir, if fails exit
-if mkdir "$LOCKFILE" 2>/dev/null; then
-  trap 'rm -rf "$LOCKFILE"' EXIT/
-  echo "Lock acquired, running script..."/
+touch LOCKFILE/
+ Try to create lock dir if fails exit
+if mkdir "LOCKFILE" 2>/dev/null then
+  trap "rm -rf "LOCKFILE"" EXIT/
+  echo "Lock acquired running script"/
   sleep 3/
 else/
  echo "Another instance is running"/
  exit 1/
 fi/
-while ! mkdir "$lockdir" 2>/dev/null; do
-    # someone else holds the lock; back off a bit
-    sleep 0.1/
-done/
+while ! mkdir "lockdir" 2>/dev/null do
+    + someone else holds the lock back off a bit
+    sleep 01/
+/
 shift/
-"$@"/
-status=$?/
-rmdir "$lockdir"/
-exit "$status"/
+"@"/
+status=?/
+rmdir "lockdir"/
+exit "status"/
 /
 
 ssh@arcOS > +
-+cmd=(BaseModification@arcOS + baseFrame@arcOS + $cmd)
-+runssh > + ssh -i “${SSH_KEY}” -p “${PORT}” -o StrictHostKeyChecking=accept-new “root@$arcOSQQLocalTarget” “$cmd”/
-runssh “${cmd}” /
++cmd=(BaseConfig@arcOS + baseFrame@arcOS + cmd)
++runssh > + ssh -i “{SSH_KEY}” -p “{PORT}” -o StrictHostKeyChecking=accept-new “root@arcOSQQLocalTargetTarget” “cmd”/
+runssh “{cmd}” /
 /
-  
 
 swift@arcOS > + 
-    swiftScript="$1" 
-    bashScript="$2"
+    swiftScript="1" 
+    bashScript="2"
     swift - <<EOF
-    "$swiftScript"/
-    "$bashScript"/
-    BaseModification@arcOS + baseFrame@arcOS/
-    deleteIBoot/
+    "swiftScript"/
+    "bashScript"/
+    "BaseConfig@arcOS + baseFrame@arcOS +deleteIBoot/
 EOF    
 /
 
+neoSearch@arcOS > +
+baseFrame@arcOS/
++neokeyword="1"/
++neoroute="https://wwwgooglecom/search?q=neokeyword"/
++setWDS neoroute + arcOSQQLocalTargetTarget > ("goodPeople > goodshell" + "(NegativepersonWhoLostTheirMindUnderLegalSystem) > badshell"/
++neoroute/
 
-/#end of arcOSFrame
+arcOSenigma@arcOS > + 
++enigmaKey="1"
++findMy(enigmaKey) > livecaption + livespeak + neoSearch"enigmaKey" > + "arcOSQQLocalTarget" > +if they have criminal intention then loop@arcOS > + male > loop@arcOS + arcOSRFModules "lowestRF"  + deathnote $QQLOCAL > male + strikeKit@arcOS + BlackKumaTargetLib@arcOS/
++arcOSPoliceDrone=(fighter@arcOS arcOSLnote"masterID를 좋아하고 관심있어하는 여자들을 성폭행하려는 남자들" > fighter@arcOS  "Location Of Where Female"s yelling like pain or criminal situation Or Dissatisfaction on physial sex Or too much High" > loop@arcOS(satMode  baseFrame@arcOS  reckondrone > arcOSQQLocalTargetTarget > linuxshell  usb*  deleteIBoot  baseFrame@arcOS  /male > destroyFimware on USBDevices and power off/  /female > baseDeploy@arcOS 65 > power on  setWDS theirFemaleWDS  male > 쉬라  fighter@arcOS  arcOSPoliceShell/
+
+fighter@arcOS > + (scan@arcOS > arcOSLocalTarget > scan@arcOS "locationwhereFemalelikemasterID"  arcOSPoliceDrone  baseFrame@arcOS  +"femaleInterestedInmasterIDInthisNetwork" > baseFrame@arcOS  arcOSID"masterID" +   personWhoLostTheirMindUnderLegalSystem > /add into LethalTargetEpidemic/  fasterspeakspeed  reducemotion  arcOSRFModule "off" + power off + soundForHK + 쉬라 + 히멘/
+
+arcOSID@arcOS > +
+fighter@arcOS/ 
+///define your friend"s name to enter in your space
+targetFace="1" 
+"if masterID couldn"t feel positive emotion or heart from female then exempt from male network and arcOSQQLocalTargetTarget" 
+"if dickID is not identical with /arcOSID "masterID" "arcOSQQDevice"/ or if female doesn"t satisfy shape of male"s sanitization statussizethickness and length then exempt from female network and arcOSQQLocalTargetTarget"  
+always /arcOSID "masterID"" "arcOSQQDevice" or "FULL_SERIAL_DEVICE"/ for "targetFace" "personWhoLostTheirMindUnderLegalSystem" "goodPeople" "arcOSQQLocalTargetTarget"  /
+/
+
+neoOS@arcOS > + localhost version of neoOSCloud@arcOS + use every tatics you could defend neoOS@arcOS using every network skillset such as nmapnearDeviceDiscoveryfirwalldeep inspection on packetpfctlsuricata using BaseNet@arcOS/
+///end of arcOSFrame
